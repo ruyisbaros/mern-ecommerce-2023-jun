418a5233edaf9b19a29626c85df719116e0a4c36
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-07-16 23:23:00 +0200

    boiler plate

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import { Routes, Router } from "react-router-dom"
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import './App.css';
 
 function App() {
   return (
     <div className="text-[red]">
-      hello
+      <ToastContainer position="bottom-center" limit={1} />
+      <Routes>
+
+      </Routes>
     </div>
   );
 }
